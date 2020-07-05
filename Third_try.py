d0df216b3fd9619e8bc884c215aa75af7804885c
Alexey Makurin <makurin-1997@mail.ru>
2020-07-05 18:56:26 +0300

    Refactored after the last call with Ivan

--- Third_try.py
+++ Third_try.py
@@ -1,72 +1,76 @@
 import json
 import os
 from optparse import OptionParser
-import itertools 
+import itertools
 import copy
 
+#input_folder = 'C:/langevin_simulator-binding/x64/Release/'
+#task_file_name = "cf2.json"
+#parent_config_filename = 'cf1.json'
+#results_folder = "C:/langevin_simulator-binding/x64/Release/results/"
 
-input_folder = "C:/langevin_simulator-binding/x64/Release/"
-task_file_name = "cf2.json"
-parent_config_filename = 'cf1.json'
-results_folder = "C:/langevin_simulator-binding/x64/Release/results/"
 
+def get_path_file(key):
+    for k, v in options.__dict__.items():
+        if k == key:
+            return v
 
-def get_task_file(path):
-    pass
-
-def patch_config(cfg):
-    pass
 
 def main():
 
-    with open(os.path.join(input_folder, task_file_name)) as f:
-	templates = json.load(f)
-	
-    j = 0
+    with open(os.path.join(get_path_file('input_folder'), get_path_file('task_file_name'))) as f:
+        templates = json.load(f)
+
+    numb_dict = 0
 
     for cross_scan_dict in templates["Task"]["CrossScan"]:
-		
+
         if cross_scan_dict != {}:
-	
-	    j = j + 1
-			
-	    type_params = [item.split("->") for item in cross_scan_dict]
 
-	    keys = dict([(key, [par[1] for par in group]) for key, group in itertools.groupby(type_params, lambda x: x[0])])
+            numb_dict += 1
 
-	    cross_scan = list([cross_scan_dict.get(item + '->' + key) for key in keys[item]] for item in keys)
+            type_params = [item.split("->") for item in cross_scan_dict]
+            type_params_grouped = {key: [par[1] for par in group]
+                                   for key, group in itertools.groupby(type_params, lambda x: x[0])}
 
-	    tmp_dict = dict([(item,[dict(zip(keys[item], comb)) for comb in itertools.product(*cross_scan[i])]) for item, i in zip(keys, range(len(keys)))])
+            # creation list values of parameters
+            cross_scan = [[cross_scan_dict[type_param + '->' + par] for par in type_params_grouped[type_param]]
+                          for type_param in type_params_grouped]
+            # creation complex dict - {'type_parameter_i': [{'name_param_j': 'value'}]},
+            # [] includes all combination for type_parameter_i
+            combination_dict = {type_par: [dict(zip(par, comb)) for comb in itertools.product(*list_values)]
+                                for (type_par, par), list_values in zip(type_params_grouped.items(), cross_scan)}
 
-	    with open(os.path.join(input_folder, parent_config_filename)) as f:
-		configuration = json.load(f)
+            with open(os.path.join(get_path_file('input_folder'), get_path_file('parent_config_filename'))) as f:
+                configuration = json.load(f)
 
-	    model_parameters = configuration
+            save_configs = [configuration]
 
-	    config = model_parameters.copy()
-	    save_config = model_parameters.copy()
+            def save(config, type_param, comb):
+                config['Configuration'][type_param].update(comb)
+                s = copy.deepcopy(config)
+                return s
 
-	    def saves(item, key, path):
-	        try:
-		    config.update(save_config)
-		except:
-		    config.update(item)
+            #creation of configuration files based on the parent config
+            for type_param in combination_dict.keys():
+                new_configs = [save(config, type_param, comb) for comb in combination_dict[type_param]
+                               for config in save_configs]
+                save_configs = copy.deepcopy(new_configs)
 
-		config['Configuration'][key].update(path)
-		s = copy.deepcopy(config)
-		return s
-
-	    for key in keys:
-	        new_config = [saves(item, key, path) for path in tmp_dict[key] for item in save_config] 
-		save_config = copy.deepcopy(new_config)
-
-	    for item, i in zip(new_config, range(len(new_config))):	
-	        #print(i+1, item)
-		with open(os.path.join(results_folder,'conf_' + str(j) + '_' + str(i + 1) + '.json'), 'w') as f:
-		    json.dump(item, f, sort_keys=True, indent=2)
+            for config, numb_comb in zip(new_configs, range(len(new_configs))):
+                with open(os.path.join(get_path_file('results_folder'), f'conf_{numb_dict}_{numb_comb + 1}.json'), 'w')\
+                        as f: json.dump(config, f, sort_keys=True, indent=2)
 
 if __name__ == '__main__':
+
+    parser = OptionParser()
+
+    parser.add_option('-i', '--input_folder')
+    parser.add_option('-t', '--task_file_name')
+    parser.add_option('-p', '--parent_config_filename')
+    parser.add_option('-r', '--results_folder')
+
+    (options, args) = parser.parse_args()
+
     main()
-    _, args = OptionParser().parse_args()
-    print(_)
-    print(args)
+
